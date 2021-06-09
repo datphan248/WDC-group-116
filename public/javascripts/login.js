2bfe2a52c41e5cb2e174fb41f46b2ce3d380b49b
Antony <a1798756@student.adelaide.edu.au>
2021-06-09 10:27:07 +0000

    Added CSS to login + completed user/manager authenticated login functionality

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,25 @@
 function display_signup() {
     window.open("https://ide-7ffa2dafa0d84506ac6daba9570f3246-8081.cs50.ws/signup.html");
 }
+
+function login() {
+
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "user" && password == "user123") {
+      alert("User validation succeeded. Welcome!");
+      location.href = "user_home.html";
+     }
+    else if (username == "manager" && password == "manager123") {
+      alert("Venue manager validation succeeded. Welcome!");
+      location.href = "manager_home.html";
+    }
+    else {
+      alert("Validation failed. Please re-enter your login details.");
+      location.href = "login.html";
+    }
+
+    return false;
+
+}
