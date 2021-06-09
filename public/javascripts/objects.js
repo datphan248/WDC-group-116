68269ee680a93cb115c90b1894b8f603f8dd8618
Antony <a1798756@student.adelaide.edu.au>
2021-06-09 03:05:49 +0000

    fix AJAX

--- public/javascripts/objects.js
+++ public/javascripts/objects.js
@@ -1,26 +1,45 @@
-let users = [];
+var user = {
+                first_name: document.getElementById('firstname').value,
+                last_name: document.getElementById('lastname').value,
+                date_of_birth: document.getElementById('birthdate').value,
+                email: document.getElementById('email').value,
+                mobileNumber: document.getElementById('mob_number').value,
+                accountCreationTime: new Date(),
+                add() { return `${this.first_name} ${this.last_name}`; },
+             };
+
 
 function add_user() {
 
-    var user = {
-        first_name: document.getElementById('firstname').value,
-        last_name: document.getElementById('lastname').value,
-        date_of_birth: document.getElementById('birthdate').value,
-        email: document.getElementById('email').value,
-        mobileNumber: document.getElementById('mob_number').value,
-        accountCreationTime: new Date()
-    };
+    var xhttp = new XMLHttpRequest();
 
-    users.push(user);
-    document.forms[0].reset();
+    xhttp.onreadystatechange = user.add();
+
+    xhttp.open("POST", "/user_home.html", true);
+    xhttp.send();
 
 }
 
-let venues = [];
+var venue = {
+                venue_name: document.getElementById('venue_name').value,
+                contact_no: document.getElementById('contact_no').value,
+                address1: document.getElementById('address1').value,
+                address2: document.getElementById('adress2').value,
+                suburb: document.getElementById('suburb').value,
+                postcode: document.getElementById('postcode').value,
+                add_venue() { return `${this.venue_name}``; },
+             };
 
 function add_venue() {
 
+    var xhttp = new XMLHttpRequest();
 
+    xhttp.onreadystatechange = venue.add();
 
+    if (venues != []) {
+        xhttp.open("POST", "/manager_home.html", true);
+        xhttp.send();
+    }
 
 }
+
