0e51ef99ba12bc635123a5d6fcc148c879e9af56
datphan248 <datphan248@gmail.com>
2021-06-10 09:10:24 +0930

    Adding Qr and package Json

---
+++ public/javascripts/qrcode.js
@@ -0,0 +1,22 @@
+
+var QRCode = require("qrcode")
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/jpeg',
+  quality: 0.3,
+  margin: 1,
+  color: {
+    dark:"#010599FF",
+    light:"#FFBF60FF"
+  }
+}
+
+QRCode.toFile('../images/a.png', 'Google.com', {
+  color: {
+    dark: '#00F',  // Blue dots
+    light: '#0000' // Transparent background
+  }
+}, function (err) {
+  if (err) throw err
+  console.log('done')
+})
