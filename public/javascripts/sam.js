1dbae01cbd916d645399954faaccb08959540dbd
samyin999 <samyin999@gmail.com>
2021-06-10 09:05:37 +0930

    fixing time getdate

--- public/javascripts/sam.js
+++ public/javascripts/sam.js
@@ -1,10 +1,15 @@
+// var dateFormat = require('dateformat');
+// var now = new Date();
+// dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
 function getDate() {
-    document.getElementById("current_time").innerHTML = new Date();
+    var d = new Date();
+    var datestring = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+    document.getElementById("current_time").innerHTML = datestring;
 }
 
-function appendDate() {
-    var currentDate = new Date();
-    let appendTarget = document.getElementById("append_time").innerHTML;
-    appendTarget.append(currentDate);
-}
-
+// function appendDate() {
+//     var currentDate = new Date();
+//     let appendTarget = document.getElementById("append_time").innerHTML;
+//     appendTarget.append(currentDate);
+// }
